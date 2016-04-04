9e60471c95a26f6fad3356d6befaea8d923ae77a
Ashley Hutton <ashley.hutton@ku.edu>
2016-04-04 18:20:12 -0500

    Add validations to models (anh)

--- classify/app/models/course.rb
+++ classify/app/models/course.rb
@@ -1,2 +1,5 @@
 class Course < ActiveRecord::Base
+
+	validates_uniqueness_of :name
+	
 end
--- classify/app/models/score.rb
+++ classify/app/models/score.rb
@@ -1,2 +1,7 @@
 class Score < ActiveRecord::Base
+
+	validates_inclusion_of		:difficulty_rating, :in => 1..5
+	validates_inclusion_of		:likeability_rating, :in => 1..5
+	validates_inclusion_of		:workload_rating, :in => 1..5
+
 end
