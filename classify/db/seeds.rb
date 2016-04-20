8c544da00ea40f68d1cabdfe068941a20ce9575c
Ashley Hutton <ashley.hutton@ku.edu>
2016-04-20 09:34:51 -0500

    Update seeds file

--- classify/db/seeds.rb
+++ classify/db/seeds.rb
@@ -13,26 +13,26 @@ school2 = School.create(name: 'Freestate')
 school3 = School.create(name: 'Andover Central')
 school4 = School.create(name: 'Junction City')
 
-course1 = Course.create(name: 'Prgramming', school_id: 1 )
-course2 = Course.create(name: 'History', school_id: 2 )
-course3 = Course.create(name: 'Geometry', school_id: 2 )
-course4 = Course.create(name: 'Cooking 1', school_id: 3 )
-course5 = Course.create(name: 'Sewing', school_id: 4 )
-course6 = Course.create(name: 'Anatomy', school_id: 4 )
+course1 = Course.create(name: 'Prgramming', school_id: Score.first )
+course2 = Course.create(name: 'History', school_id: Score.first )
+course3 = Course.create(name: 'Geometry', school_id: Score.first )
+course4 = Course.create(name: 'Cooking 1', school_id: Score.first )
+course5 = Course.create(name: 'Sewing', school_id: Score.first )
+course6 = Course.create(name: 'Anatomy', school_id: Score.first )
 
 score1 = Score.create(difficulty_rating: '3', workload_rating: '3', likeability_rating: '3',
-	comment: 'This class was a-okay', course_id: '1')
+	comment: 'This class was a-okay', course_id: Course.first)
 score2 = Score.create(difficulty_rating: '1', workload_rating: '1', likeability_rating: '1',
-	comment: 'This class sucked eggs', course_id: '2')
+	comment: 'This class sucked eggs', course_id: Course.first)
 score3 = Score.create(difficulty_rating: '5', workload_rating: '5', likeability_rating: '5',
-	comment: 'This class was my fave. ', course_id: '3')
+	comment: 'This class was my fave. ', course_id: Course.first)
 score4 = Score.create(difficulty_rating: '5', workload_rating: '5', likeability_rating: '1',
-	comment: 'Not a lot of work but I did not like it', course_id: '4')
+	comment: 'Not a lot of work but I did not like it', course_id: Course.first)
 score5 = Score.create(difficulty_rating: '2', workload_rating: '2', likeability_rating: '2',
-	comment: 'This course was eh', course_id: '5')
+	comment: 'This course was eh', course_id: Course.first)
 score6 = Score.create(difficulty_rating: '4', workload_rating: '4', likeability_rating: '4',
-	comment: 'Pretty good pretty good', course_id: '6')
+	comment: 'Pretty good pretty good', course_id: Course.first)
 score7 = Score.create(difficulty_rating: '3', workload_rating: '3', likeability_rating: '3',
-	comment: 'This class was a-okay', course_id: '1')
+	comment: 'This class was a-okay', course_id: Course.first)
 score8 = Score.create(difficulty_rating: '1', workload_rating: '1', likeability_rating: '3',
-	comment: 'I liked it but it was super hard', course_id: '2')
+	comment: 'I liked it but it was super hard', course_id: Course.first)
