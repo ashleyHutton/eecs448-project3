fd86031ca11c6737ea8245691b18686cc7a82ad3
Hannah Johnson <hlj.inbox@gmail.com>
2016-05-03 10:05:16 -0500

    Update user home pages, logo, and add deployment plan essay.

--- classify/config/routes.rb
+++ classify/config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
 
   root to: 'scores#index'
 
+  devise_scope :user do
+    get 'users/sign_out' => "devise/sessions#destroy"
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
