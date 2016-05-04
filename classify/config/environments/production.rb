b7f2ca8607585af2b54531919d481a4bb505f2f0
Ashley Hutton <ashley.hutton@ku.edu>
2016-05-04 08:14:18 -0500

    Add secret keys

--- classify/config/environments/production.rb
+++ classify/config/environments/production.rb
@@ -24,6 +24,8 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
@@ -34,6 +36,8 @@ Rails.application.configure do
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
+
+  config.assets.initialize_on_precompile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
