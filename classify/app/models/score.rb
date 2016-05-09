fa8b6b0884f63ace96f36b27ec84477e163c92d7
Rabel Marte <rabelmarte@ku.edu>
2016-05-08 20:27:27 -0500

    Allow the user to only make one score per course

--- classify/app/models/score.rb
+++ classify/app/models/score.rb
@@ -13,6 +13,7 @@ class Score < ActiveRecord::Base
 	validates_inclusion_of		:difficulty_rating, :in => 1..5
 	validates_inclusion_of		:likeability_rating, :in => 1..5
 	validates_inclusion_of		:workload_rating, :in => 1..5
+	validates :creator_id, uniqueness: {scope: :course_id}
 
 	scope :sorted_by, -> (id) { where(:id => id) }
 
