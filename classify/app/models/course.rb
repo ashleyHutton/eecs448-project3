7b9296420a7651a357eff1ed5449c7de902d1941
Ashley Hutton <ashley.hutton@ku.edu>
2016-05-08 20:36:00 -0500

    Add search functionality for courses

--- classify/app/models/course.rb
+++ classify/app/models/course.rb
@@ -16,9 +16,35 @@ class Course < ActiveRecord::Base
 		  ]
 		)
 
-	def self.options_for_select
-	  order('LOWER(name)').map { |e| [e.name, e.id] }
-	end
+	scope :sorted_by, lambda { |sort_option|
+	# extract the sort direction from the param value.
+		direction = (sort_option =~ /desc$/) ? 'desc' : 'asc'
+		case sort_option.to_s
+			when /^difficulty_rating_/
+				# Simple sort on the created_at column.
+				# Make sure to include the table name to avoid ambiguous column names.
+				# Joining on other tables is quite common in Filterrific, and almost
+				# every ActiveRecord table has a 'created_at' column.
+				order("courses.scores.difficulty_rating #{ direction }")
+			when /^workload_rating_/
+				# Simple sort on the name colums
+				order("courses.scores.workload_rating #{ direction }")
+			when /^likeability_rating_/
+				# This sorts by a student's country name, so we need to include
+				# the country. We can't use JOIN since not all students might have
+				# a country.
+				order("courses.scores.likeability_rating #{ direction }")
+		else
+			raise(ArgumentError, "Invalid sort option: #{ sort_option.inspect }")
+		end
+	}
 
+  def self.options_for_sorted_by
+    [
+      ['Least Difficult', 'difficulty_rating_asc'],
+      ['Least Workload', 'workload_rating_asc'],
+      ['Most Liked', 'likeability_rating_desc']
+    ]
+  end
 
 end
