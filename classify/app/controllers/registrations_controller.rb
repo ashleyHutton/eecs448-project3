0f89cfa3c1ed59802e19bcaa132cd11d816b0711
Hannah Johnson <hlj.inbox@gmail.com>
2016-05-10 22:13:10 -0500

    Created documentation

--- classify/app/controllers/registrations_controller.rb
+++ classify/app/controllers/registrations_controller.rb
@@ -1,3 +1,5 @@
+##
+# Registrations Controller
 class RegistrationsController < Devise::RegistrationsController
 # before_filter :configure_sign_up_params, only: [:create]
 # before_filter :configure_account_update_params, only: [:update]
--- classify/app/models/user.rb
+++ classify/app/models/user.rb
@@ -1,3 +1,5 @@
+##
+# User Model
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
